463ed156065d5fe4b7eea15834dc57e6bac119dc
Adrian <adrian.marcos.02@gmail.com>
2024-02-19 17:34:05 +0100

    Customed some files

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1708359520',resources: [
+    cacheName: 'chirpy-1708360431',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
