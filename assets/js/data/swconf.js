581d312ff6372849d070ade1cde85c88369599b4
Adrian <adrian.marcos.02@gmail.com>
2024-11-17 22:09:36 -0500

    Added carousel and some pictures

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731685277',resources: [
+    cacheName: 'chirpy-1731899321',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
