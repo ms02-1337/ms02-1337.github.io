edc83392231c5b1d61ddb64a8fa53a69f41f3a0c
Adrian Marcos <adrian.marcos.02@gmail.com>
2025-01-28 17:23:36 +0100

    Updated jekyll library

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1731899524',resources: [
+    cacheName: 'chirpy-1738081332',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
