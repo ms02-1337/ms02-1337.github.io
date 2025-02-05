54937d15ab11463a0a0bd1101b76148e48523e26
Adrian Marcos <adrian.marcos.02@gmail.com>
2025-02-05 10:12:28 +0100

    Updated electric motorbike post

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738082730',resources: [
+    cacheName: 'chirpy-1738746730',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
