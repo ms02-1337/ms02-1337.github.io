23a0e7a97eb717c4390f5e8da62a407af146b934
Adrian Marcos <adrian.marcos.02@gmail.com>
2026-04-19 18:31:16 +0200

    New blog post: Dynamometer from scratch

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1753262835',resources: [
+    cacheName: 'chirpy-1776616181',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
