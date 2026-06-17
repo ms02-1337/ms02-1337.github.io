b444aafeeea90898b5a165a6a600ac0c9ede6d83
Adrian Marcos <adrian.marcos.02@gmail.com>
2026-06-17 18:21:52 +0200

    Removed drafts

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1781712582',resources: [
+    cacheName: 'chirpy-1781713299',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
